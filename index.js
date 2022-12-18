c772ce6643546d35c803e64755040f4178351d05
Rostyslav Onikiienko <r.onikienko@gmail.com>
2022-12-18 15:20:29 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom/client'
+import {App} from './src/components/App/App';
+
+ReactDom.createRoot(document.getElementById('root')).render(<App />)
